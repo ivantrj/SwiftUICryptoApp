00456b82f9cdcb0feb8580d22e51074ae848084c
ivan <ivantrajanovski@gmail.com>
2024-02-02 11:24:50 +0100

    create coin row view

---
+++ SwiftUICrypto/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,27 @@
+//
+//  CoinRowView.swift
+//  SwiftUICrypto
+//
+//  Created by ivan trj  on 02.02.24.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    let coin: CoinModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+//#Preview {
+//    CoinRowView(coin: dev.coin)
+//}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView(coin: dev.coin)
+    }
+}
