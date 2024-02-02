ace47780998cd9d8f0767b38ae4063893b537852
ivan <ivantrajanovski@gmail.com>
2024-02-02 11:23:07 +0100

    create extension for preview

---
+++ SwiftUICrypto/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinRowView.swift
+//  SwiftUICrypto
+//
+//  Created by ivan ruwido  on 02.02.24.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinRowView()
+}
